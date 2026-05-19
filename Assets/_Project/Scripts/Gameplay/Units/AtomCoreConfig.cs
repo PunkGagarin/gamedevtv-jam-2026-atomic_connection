4221a26ff5da99d318ab25b9389eac1616d0698b
RomanT <roman.tara@holydaystudios.com>
2026-05-19 11:25:18 +0700

    config rename (Click -> CoreConfig

---
+++ Assets/_Project/Scripts/Gameplay/Units/AtomCoreConfig.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay.Units
+{
+    [CreateAssetMenu(fileName = "AtomCoreConfig", menuName = "Game Resources/Configs/Atom Core")]
+    public class AtomCoreConfig : ScriptableObject
+    {
+        [field: Header("Core")]
+        [field: SerializeField, Min(1)] public int CoreMaxHealth { get; private set; } = 2;
+
+        [field: Header("Atom Production")]
+        [field: SerializeField, Min(1)] public int ClicksToGenerateFreeAtom { get; private set; } = 4;
+
+        [field: Header("Free Atom Placement")]
+        [field: SerializeField, Min(0f)] public float SpawnRadiusOffset { get; private set; } = 0.6f;
+
+        [field: Header("Free Atom Orbit")]
+        [field: SerializeField, Min(0f)] public float FreeAtomOrbitDegreesPerSecond { get; private set; } = 60f;
+    }
+}
